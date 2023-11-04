52303a64f1675841c3dc9170e7f23599f257718f
ndmsnkri <144985362+ndmsnkri@users.noreply.github.com>
2023-11-04 23:23:24 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -18,12 +18,8 @@ public class Main {
             System.out.print(value + " | ");
         }
 
-
-        // Calculate and display the sum and average of dataPoints
-        int sum = 0;
-        for (int value : dataPoints) {
-            sum += value; // sum = sum + value
-        }
+        // Calculate and display the sum and average of dataPoints using the static methods
+        int sum = sum(dataPoints);
         double average = (double) sum / dataPoints.length;
         System.out.println("\nThe average of the random array dataPoints is: " + average);
         System.out.println("The sum of the random array dataPoints is: " + sum);
@@ -31,50 +27,85 @@ public class Main {
         // Linear scan (search)
         int userValue1 = SafeInput.getRangedInt(in, "Enter an integer", 1, 100);
 
-        int count = 0;
-        for (int value : dataPoints) {
-            if (value == userValue1) {
-                count++;
-            }
-        }
+        int count = occuranceScan(dataPoints, userValue1);
         System.out.println("The user's value " + userValue1 + " was found " + count + " times in the array.");
 
         int userValue2 = SafeInput.getRangedInt(in, "Enter another integer", 1, 100);
 
-        int position = -1;
-        for (int i = 0; i < dataPoints.length; i++) {
-            if (dataPoints[i] == userValue2) {
-                position = i;
-                break;
-            }
-        }
+        int position = findFirstOccurrence(dataPoints, userValue2);
         if (position != -1) {
             System.out.println("The value " + userValue2 + " was found at array index " + position);
         } else {
             System.out.println("The value " + userValue2 + " was not found in the array.");
         }
 
-        int min = dataPoints[0];
-        int max = dataPoints[0];
-        for (int value : dataPoints) {
-            if (value < min) {
-                min = value;
-            }
-            if (value > max) {
-                max = value;
-            }
-        }
-        System.out.println("The minimum value in dataPoints is: " + min);
-        System.out.println("The maximum value in dataPoints is: " + max);
+        int minValue = min(dataPoints);
+        int maxValue = max(dataPoints);
+
+        System.out.println("The minimum value in dataPoints is: " + minValue);
+        System.out.println("The maximum value in dataPoints is: " + maxValue);
 
         System.out.println("\nAverage of dataPoints is: " + getAverage(dataPoints));
     }
 
     public static double getAverage(int[] values) {
+        int sum = sum(values);
+        return (double) sum / values.length;
+    }
+
+    public static int min(int[] values) {
+        int min = values[0];
+        for (int value : values) {
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    public static int max(int[] values) {
+        int max = values[0];
+        for (int value : values) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    public static int occuranceScan(int[] values, int target) {
+        int count = 0;
+        for (int value : values) {
+            if (value == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int sum(int[] values) {
         int sum = 0;
         for (int value : values) {
             sum += value;
         }
-        return (double) sum / values.length;
+        return sum;
+    }
+
+    public static boolean contains(int[] values, int target) {
+        for (int value : values) {
+            if (value == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int findFirstOccurrence(int[] values, int target) {
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] == target) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
